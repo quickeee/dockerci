2b048efa95316fbc0a463a2e5bff3b89b6c3c23b
mohamed <mohamed.a@y3technologies.com>
2016-11-23 14:21:26 +0800

    express upgrade

--- app.js
+++ app.js
@@ -5,12 +5,21 @@
 var express = require('express'),
     app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    res.render('home', {
-  });
+//     res.render('home', {
+//   });
+res.sendFile("index.html", { root: __dirname });
+
 });
 
 app.listen(8080);
